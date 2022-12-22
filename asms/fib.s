e4740f1fdf0725e601c42393778f6fb957ebc8d2
Kanehiro103 <kobayashi-daiki103@g.ecc.u-tokyo.ac.jp>
2022-12-22 15:21:34 +0900

    ディスアセンブラ追加

---
+++ asms/fib.s
@@ -0,0 +1,11 @@
+       0: addi r0 r255 1
+       1: addi r1 r255 1
+       2: addi r2 r255 0
+       3: addi r10 r255 0
+       4: addi r11 r255 20
+       5: add r2 r255 r1
+       6: add r1 r255 r0
+       7: add r0 r1 r2
+       8: addi r10 r10 1
+       9: blt r10 r11 -4
+      10: j 0
--- main.c
+++ main.c
@@ -9,6 +9,7 @@
 #include "error.h"
 #include "fpu.h"
 #include "library.h"
+#include "disassembler.h"
 
 int main(int argc, char* argv[]) {
     char* samples = "./samples/";
@@ -28,105 +29,123 @@ int main(int argc, char* argv[]) {
 
     if(argc <= 1) {
         printf("Usage: %s <filename>\n", argv[0]);
-        return 0;
-    }
-
-    sprintf(read_file, "%s%s%s", samples, argv[1], dat);
-
-    reg_set* rs;
-    rs = (reg_set*)malloc(sizeof(reg_set));
-    init_rs(rs);
-
-    rs->mem[0xfffffff8] = 1024;
-
-    if(read(rs, read_file)) {
-        printf("file not found\n");
         exit(1);
     }
 
-    while(1) {
-        printf("stdout?   y/n : ");
-        scanf("%s", s);
-        if(strcmp(s, "y") == 0) {
-            flgs.pflag = 1;
-            break;
-        } else if(strcmp(s, "n") == 0) {
-            break;
-        } else {
-            continue;
-        }
-    }
-
-    while(1) {
-        printf("logfile?  y/n : ");
-        scanf("%s", s);
-        if(strcmp(s, "y") == 0) {
-            flgs.lflag = 1;
-            break;
-        } else if(strcmp(s, "n") == 0) {
-            break;
-        } else {
-            continue;
-        }
-    }
-
-    while(1) {
-        printf("uart?     y/n : ");
-        scanf("%s", s);
-        if(strcmp(s, "y") == 0) {
-            flgs.uflag = 1;
-            break;
-        } else if(strcmp(s, "n") == 0) {
-            break;
-        } else {
-            continue;
-        }
-    }
-
-    if(flgs.lflag) {
-        char* logfiles = "./logfiles/log_";
-        char log_file[128];
-
-        sprintf(log_file, "%s%s%s", logfiles, argv[1], dat);
-
-        fps.fpl = fopen(log_file, "w");
-        if(fps.fpl == NULL) {
-            printf("faild to open log_file\n");
+    if(strcmp(argv[1], "#disasm") == 0) {
+        char fname[128];
+        printf("filename? ");
+        scanf("%s", fname);
+        if(disassemble(fname)) {
+            printf("file not found\n");
             exit(1);
-        } else {
-            opl = 1;
         }
-    }
+    } else {
+        sprintf(read_file, "%s%s%s", samples, argv[1], dat);
 
-    if(flgs.uflag) {
-        char* uartfiles = "./uart/uart_";
-        char uart_file[128];
+        reg_set* rs;
+        rs = (reg_set*)malloc(sizeof(reg_set));
+        init_rs(rs);
 
-        sprintf(uart_file, "%s%s%s", uartfiles, argv[1], dat);
+        rs->mem[0xfffffff8] = 1024;
 
-        fps.fpu = fopen(uart_file, "w");
-        if(fps.fpu == NULL) {
-            printf("faild to open log_file\n");
+        if(read(rs, read_file)) {
+            printf("file not found\n");
             exit(1);
-        } else {
-            opu = 1;
+        }
+
+        while(1) {
+            printf("stdout?   y/n : ");
+            scanf("%s", s);
+            if(strcmp(s, "y") == 0) {
+                flgs.pflag = 1;
+                break;
+            } else if(strcmp(s, "n") == 0) {
+                break;
+            } else {
+                continue;
+            }
+        }
+
+        while(1) {
+            printf("logfile?  y/n : ");
+            scanf("%s", s);
+            if(strcmp(s, "y") == 0) {
+                flgs.lflag = 1;
+                break;
+            } else if(strcmp(s, "n") == 0) {
+                break;
+            } else {
+                continue;
+            }
+        }
+
+        while(1) {
+            printf("uart?     y/n : ");
+            scanf("%s", s);
+            if(strcmp(s, "y") == 0) {
+                flgs.uflag = 1;
+                break;
+            } else if(strcmp(s, "n") == 0) {
+                break;
+            } else {
+                continue;
+            }
+        }
+
+        if(flgs.lflag) {
+            char* logfiles = "./logfiles/log_";
+            char log_file[128];
+
+            sprintf(log_file, "%s%s%s", logfiles, argv[1], dat);
+
+            fps.fpl = fopen(log_file, "w");
+            if(fps.fpl == NULL) {
+                printf("faild to open log_file\n");
+                exit(1);
+            } else {
+                opl = 1;
+            }
+        }
+
+        if(flgs.uflag) {
+            char* uartfiles = "./uart/uart_";
+            char uart_file[128];
+
+            sprintf(uart_file, "%s%s%s", uartfiles, argv[1], dat);
+
+            fps.fpu = fopen(uart_file, "w");
+            if(fps.fpu == NULL) {
+                printf("faild to open log_file\n");
+                exit(1);
+            } else {
+                opu = 1;
+            }
+        }
+
+        while(!flag) {
+            exec(rs, rs->mem[rs->pc], &fps, &flag, &flgs);
+        }
+        print_reg(rs);
+
+        free_rs(rs);
+
+        if(opl) {
+            fclose(fps.fpl);
+        }
+
+        if(opu) {
+            fclose(fps.fpu);
         }
     }
 
-    while(!flag) {
-        exec(rs, rs->mem[rs->pc], &fps, &flag, &flgs);
-    }
-    print_reg(rs);
+    /*
+    union { float f; unsigned int i; } a;
 
-    free_rs(rs);
-
-    if(opl) {
-        fclose(fps.fpl);
-    }
-
-    if(opu) {
-        fclose(fps.fpu);
-    }
-
+    a.f = 4.0 * 8.0 + 5.0 * 10.0 + 6.0 * 12.0;
+    //scanf("%f", &a.f);
+    //scanf("%f", &b.f);
+    printf("%d\n", ftoi_num(a.i));
+    */
     return 0;
 }
